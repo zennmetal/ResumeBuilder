21a11004b416ae61f1636a53a8b93214fe504516
Matt Mauro <matt.mauro@gmail.com>
2019-12-09 14:54:13 -0500

    first commit

---
+++ ResumeBuilder/ResumeDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResumeBuilder
+//
+//  Created by Matthew Mauro on 2019-12-09.
+//  Copyright © 2019 Matthew Mauro. All rights reserved.
+//
+
+import UIKit
+
+class ResumeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
